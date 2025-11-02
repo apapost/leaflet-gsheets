c0293437ccbd7c0ee013b08461e65b16236497f4
APOSTOLOS APOSTOLIDIS <146770489+apapost@users.noreply.github.com>
2025-11-02 17:13:12 +0200

    Update main.js

--- main.js
+++ main.js
@@ -54,14 +54,14 @@ map.on('locationfound', function(e) {
   console.log("Θέση χρήστη:", userLat, userLng);
 
 // Εμφάνιση κύκλου γύρω από τη θέση του χρήστη (χωρική επίγνωση)
-L.circle([userLat, userLng], {
+const userCircle = L.circle([userLat, userLng], {
   radius: 120 * 1000, // ακτίνα σε μέτρα (120 km)
   color: "blue",
   fillColor: "#aaddff",
   fillOpacity: 0.2
 }).addTo(map);
 
-   //console.log("Ακτίνα κύκλου χρήστη:", userCircle.getradius());
+  console.log("Ακτίνα κύκλου χρήστη:", userCircle.getRadius());
   // Φόρτωσε τα σημεία από το CSV και φίλτραρε
   Papa.parse(pointsURL, {
     download: true,
